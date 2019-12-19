3790a0c7f17b085645ad5f429464a6b98474226a
Akshay Arora <akshay575@yahoo.com>
2019-12-19 23:30:52 +0530

    update users returned by company type schema

--- schema/schema.js
+++ schema/schema.js
@@ -16,7 +16,7 @@ const CompanyType = new GraphQLObjectType({
         name: { type: GraphQLString },
         description: { type: GraphQLString },
         users: {
-            type: new GraphQLList(CompanyType),
+            type: new GraphQLList(UserType),
             resolve(parentValue, args) {
                 return axios.get(`http://localhost:3000/companies/${parentValue.id}/users`)
                     .then(res => res.data);
